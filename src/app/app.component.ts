8ba5fc28f3a1f4413b8e409d89163a5e9c253cd9
TatoBig <tatobig@gmail.com>
2022-09-12 17:10:04 -0600

    Initial configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular-test';
+  title: string = 'Props';
+  onSave = () => {
+    console.log('test')
+  };
+  buttonStatus: boolean = false;
+  toggleButton = () => {
+    this.buttonStatus = !this.buttonStatus
+  };
 }
